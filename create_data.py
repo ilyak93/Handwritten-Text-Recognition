95a5774358e4d461c8462f7a2c665cc8ff90ad48
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-25 17:54:17 +0200

    words generation with placement diversity
    + testing the total bytes size in the lmdb

--- create_data.py
+++ create_data.py
@@ -157,6 +157,7 @@ def create_words_data(rel_in_path, rel_out_path, map_size, format, words_num):
     bg = './bg.jpg';
     bg = Image.open(bg)
     bg = np.asarray(bg)
+    total_size = 0
     for i in range (words_num):
         one_or_two = np.random.randint(0, 10)
         if one_or_two == 3:
@@ -172,7 +173,6 @@ def create_words_data(rel_in_path, rel_out_path, map_size, format, words_num):
         width = 0
 
         label = ''
-
         for i in range(word_len-1):
             letter = np.random.randint(22)
             if non_ending_letters[letter] in low_letters:
@@ -252,6 +252,7 @@ def create_words_data(rel_in_path, rel_out_path, map_size, format, words_num):
         with io.BytesIO() as output:
             background.save(output, format=output_format)
             imageBin = output.getvalue()
+            total_size += len(imageBin)
         cache[imageKey] = imageBin
         cache[labelKey] = label.encode()
         if cnt % 1000 == 0:
@@ -262,12 +263,161 @@ def create_words_data(rel_in_path, rel_out_path, map_size, format, words_num):
     nSamples = cnt - 1
     cache['num-samples'.encode()] = str(nSamples).encode()
     writeCache(env, cache)
-    print('Created dataset with %d samples' % nSamples)
+    print('Created dataset with %d samples of size % bytes' % nSamples, total_size)
 
 
-    print('hello')
+#with deversity in space
+def create_words_data2(rel_in_path, rel_out_path, map_size, format, words_num):
+    dir_in = './' + rel_in_path
+    dir_out = './' + rel_out_path;
+
+    output_format = format
+
+    os.makedirs(dir_out, exist_ok=True)
+    env = lmdb.open(dir_out, map_size=map_size)
+    cache = {}
+    cnt = 1
 
 
+    dir_in_path = pathlib.Path(dir_in);
+    chr_dirs = os.listdir(dir_in_path)
+    idx_to_chr_path = defaultdict(def_value)
+    aleph = ord('א')
+    for chr_dir in chr_dirs:
+        print(chr(int(chr_dir) + aleph))
+        chr_dir_path = dir_in + chr_dir
+        cur_chr_path = pathlib.Path(chr_dir_path);
+        chr_files = os.listdir(cur_chr_path)
+        for chr_file in chr_files:
+            cur_chr_img_path = chr_dir_path+'/'+chr_file
+            idx_to_chr_path[int(chr_dir)].append(cur_chr_img_path)
+
+
+    non_ending_letters = 'אבגדהוזחטיכלמנסעפצקרשת'
+    ending_letters = 'אבגדהוזחטיךלמןסעףץקרשת'
+    low_letters='נקךןת'
+
+    bg = './bg.jpg';
+    bg = Image.open(bg)
+    bg = np.asarray(bg)
+    for i in range (words_num):
+        one_or_two = np.random.randint(0, 10)
+        if one_or_two == 3:
+            word_len = np.random.randint(11, 20)
+        else:
+            word_len = np.random.randint(1, 11)
+
+        is_lower = np.zeros(word_len)
+
+        images = []
+        length_upper = 0
+        length_lower = 0
+        width = 0
+
+        label = ''
+        total_size = 0
+        #letters generations separetly
+        for i in range(word_len-1):
+            letter = np.random.randint(22)
+            if non_ending_letters[letter] in low_letters:
+                is_lower[word_len-1-i] = 1
+            letter_idx = ord(non_ending_letters[letter]) - ord('א')
+            label += chr(letter_idx+ord('א'))
+            letter_imgs = idx_to_chr_path[letter_idx]
+            amount = len(letter_imgs)
+            img_num = np.random.randint(amount-1)
+            letter_img_path = letter_imgs[img_num]
+
+            cur_chr_img = Image.open(letter_img_path)
+            cur_chr_img = np.asarray(cur_chr_img)
+            cur_length, cur_width, channels = cur_chr_img.shape
+            if channels == 4:  # remove the alpha channel
+                cur_chr_img = cur_chr_img[:, :, :-1]
+            width += cur_width
+            images.append(cur_chr_img)
+            if non_ending_letters[letter] in low_letters:
+                length_lower = max(length_lower, cur_length)
+            else:
+                length_upper = max(length_upper, cur_length)
+
+        #last letter in ending letters
+        letter = np.random.randint(22)
+        if ending_letters[letter] in low_letters:
+            is_lower[0] = 1
+        letter_idx = ord(ending_letters[letter]) - ord('א')
+        label += chr(letter_idx + ord('א'))
+        letter_imgs = idx_to_chr_path[letter_idx]
+        amount = len(letter_imgs)
+        img_num = np.random.randint(amount - 1)
+        letter_img_path = letter_imgs[img_num]
+
+        cur_chr_img = Image.open(letter_img_path)
+        cur_chr_img = np.asarray(cur_chr_img)
+        cur_length, cur_width, channels = cur_chr_img.shape
+        if channels == 4:  # remove the alpha channel
+            cur_chr_img = cur_chr_img[:, :, :-1]
+        width += cur_width
+        images.append(cur_chr_img)
+        if non_ending_letters[letter] in low_letters:
+            length_lower = max(length_lower, cur_length)
+        else:
+            length_upper = max(length_upper, cur_length)
+
+        if length_upper > length_lower:
+            total_length = ceil(length_upper + length_lower/2)
+        else:
+            total_length = ceil(length_lower + length_upper / 2)
+
+
+        #word generation from letters
+        #print(label)
+        background = np.copy(bg[0:total_length, 0:width, :])
+        width_start_idx = 0
+        for letter_img, is_low in zip(reversed(images), is_lower):
+            cur_len, cur_width, _ = letter_img.shape
+            if not is_low:
+                start_idx = length_upper - cur_len
+            else:
+                start_idx = total_length - cur_len
+            col_end_idx = width_start_idx+cur_width
+            background[start_idx:(start_idx+cur_len),
+                width_start_idx:(col_end_idx), :] = letter_img
+            width_start_idx += cur_width
+
+        background_img = Image.fromarray(background)
+        ImageShow.show(background_img)
+        print()
+        #save image in lmdb
+        #_, format = chr_file.split('.')
+        #if output_format == 'same':
+            #output_format = format
+        # word writing to lmdb in compressed form with bytesIO - reduces total size
+        imageKey = 'image-%09d'.encode() % cnt
+        labelKey = 'label-%09d'.encode() % cnt
+        background_img = Image.fromarray(background)
+        with io.BytesIO() as output:
+            background_img.save(output, format=output_format)
+            imageBin = output.getvalue()
+            total_size += len(imageBin)
+        cache[imageKey] = imageBin
+        cache[labelKey] = label.encode()
+        if cnt % 1000 == 0:
+            writeCache(env, cache)
+            cache = {}
+            print('Written %d' % cnt)
+        cnt += 1
+        word_l, word_w, _ = background.shape
+        #generation with space diversity
+        background2 = np.copy(bg[0:int(total_length*2), 0:int(width*2), :])
+        background2[0:word_l, 0:word_w, :] = background
+        background2_img = Image.fromarray(background)
+        ImageShow.show(background2_img)
+        print()
+
+    nSamples = cnt - 1
+    cache['num-samples'.encode()] = str(nSamples).encode()
+    writeCache(env, cache)
+    print('Created dataset with %d samples of size % bytes' % nSamples, total_size)
 
 if __name__ == '__main__':
 
@@ -288,8 +438,7 @@ if __name__ == '__main__':
         words_num = 100000
         print('creating lmdb for train data')
         create_words_data('/hhd_dataset/train/', './lmdb/train/', 4000000000, 'jpeg', words_num)
-    words_num = 100000
-    print('creating lmdb for validation data')
-    create_words_data('/hhd_dataset/val/', './lmdb/val/', 400000000, 'jpeg', int(words_num / 10))
+        print('creating lmdb for validation data')
+        create_words_data('/hhd_dataset/val/', './lmdb/val/', 1200000000, 'jpeg', int(words_num / 10))
 
 
