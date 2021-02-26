b573791a9ffc831f09293b1dbdd39c7324b3d4f7
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2021-02-26 22:32:34 +0200

    write to Gdrive

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def train(opt):
     opt.batch_ratio = opt.batch_ratio.split('-')
     train_dataset = Batch_Balanced_Dataset(opt)
 
-    log = open(f'/content/drive/MyDrive/{opt.exp_name}/log_dataset.txt', 'a')
+    log = open(f'/content/drive/MyDrive/saved_models/{opt.exp_name}/log_dataset.txt', 'a')
     AlignCollate_valid = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
     valid_dataset, valid_dataset_log = hierarchical_dataset(root=opt.valid_data, opt=opt)
     valid_loader = torch.utils.data.DataLoader(
@@ -121,7 +121,7 @@ def train(opt):
 
     """ final options """
     # print(opt)
-    with open(f'/content/drive/MyDrive/{opt.exp_name}/opt.txt', 'a', encoding='utf-8') as opt_file:
+    with open(f'/content/drive/MyDrive/saved_models/{opt.exp_name}/opt.txt', 'a', encoding='utf-8') as opt_file:
         opt_log = '------------ Options -------------\n'
         args = vars(opt)
         for k, v in args.items():
@@ -180,7 +180,7 @@ def train(opt):
         if (iteration + 1) % opt.valInterval == 0 or iteration == 0:  # To see training progress, we also conduct validation when 'iteration == 0'
             elapsed_time = time.time() - start_time
             # for log
-            with open(f'/content/drive/MyDrive/{opt.exp_name}/log_train.txt', 'a', encoding='utf-8') as log:
+            with open(f'/content/drive/MyDrive/saved_models/{opt.exp_name}/log_train.txt', 'a', encoding='utf-8') as log:
                 model.eval()
                 with torch.no_grad():
                     valid_loss, current_accuracy, current_norm_ED, preds, confidence_score, labels, infer_time, length_of_data = validation(
@@ -196,10 +196,10 @@ def train(opt):
                 # keep best accuracy model (on valid dataset)
                 if current_accuracy > best_accuracy:
                     best_accuracy = current_accuracy
-                    torch.save(model.state_dict(), f'/content/drive/MyDrive/{opt.exp_name}/best_accuracy.pth')
+                    torch.save(model.state_dict(), f'/content/drive/MyDrive/saved_models//{opt.exp_name}/best_accuracy.pth')
                 if current_norm_ED > best_norm_ED:
                     best_norm_ED = current_norm_ED
-                    torch.save(model.state_dict(), f'/content/drive/MyDrive/{opt.exp_name}/best_norm_ED.pth')
+                    torch.save(model.state_dict(), f'/content/drive/MyDrive/saved_models//{opt.exp_name}/best_norm_ED.pth')
                 best_model_log = f'{"Best_accuracy":17s}: {best_accuracy:0.3f}, {"Best_norm_ED":17s}: {best_norm_ED:0.2f}'
 
                 loss_model_log = f'{loss_log}\n{current_model_log}\n{best_model_log}'
@@ -224,7 +224,7 @@ def train(opt):
         # save model per 1e+5 iter.
         if (iteration + 1) % 1e+5 == 0:
             torch.save(
-                model.state_dict(), f'/content/drive/MyDrive/{opt.exp_name}/iter_{iteration + 1}.pth')
+                model.state_dict(), f'/content/drive/MyDrive/saved_models//{opt.exp_name}/iter_{iteration + 1}.pth')
 
         if (iteration + 1) == opt.num_iter:
             print('end the training')
@@ -299,7 +299,7 @@ if __name__ == '__main__':
         opt.exp_name += f'-Seed{opt.manualSeed}'
         # print(opt.exp_name)
 
-    os.makedirs(f'/content/drive/MyDrive/{opt.exp_name}', exist_ok=True)
+    os.makedirs(f'/content/drive/MyDrive/saved_models/{opt.exp_name}', exist_ok=True)
 
     """ vocab / character number configuration """
     if opt.sensitive:
